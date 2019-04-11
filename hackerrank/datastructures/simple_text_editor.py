c91343b5474cb5b8ac337acaa4b4d405c60840f2
Vivek Kumar <vivek.ispat@gmail.com>
2019-04-11 02:36:04 -0700

    update simple text editor with string concatenation

--- hackerrank/datastructures/simple_text_editor.py
+++ hackerrank/datastructures/simple_text_editor.py
@@ -1,20 +1,17 @@
 class Editor:
 
     def __init__(self):
-        self.s = []
+        self.s = ''
         self.op_history = []
 
     def append(self, w):
-        w = list(w)
         wn = len(w)
         self.op_history.append((1, wn))
-        self.s.extend(w)
+        self.s += w
 
     def delete(self, k):
         deleted = self.s[-k:]
-        for j in range(k):
-            self.s.pop()
-
+        self.s = self.s[:-k]
         self.op_history.append((2, deleted))
 
     def print(self, k):
@@ -24,10 +21,9 @@ class Editor:
         op = self.op_history.pop()
         if op[0] == 1:
             k = op[1]
-            for j in range(k):
-                self.s.pop()
+            self.s = self.s[:-k]
         elif op[0] == 2:
-            self.s.extend(op[1])
+            self.s += op[1]
 
 
 if __name__ == '__main__':
